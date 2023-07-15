37c4698bdfd823b7c7d110f8833ea66115208880
DAN-KOTI\Daniel <daniel.blombergof@gmail.com>
2023-07-15 10:16:29 +0300

    add tooltips for summoner spells

--- components/Images/SummonerSpellIcon.tsx
+++ components/Images/SummonerSpellIcon.tsx
@@ -1,4 +1,6 @@
+import Tippy from "@tippyjs/react";
 import Image from "next/image";
+import "tippy.js/dist/tippy.css"; // optional
 import SummonerSpells from "../../utils/summoner.json";
 
 interface Props {
@@ -7,27 +9,47 @@ interface Props {
 }
 
 const SummonerSpellIcon: React.FC<Props> = ({ gameVersion, id }) => {
+  let spellId = "";
   let spellName = "";
+  let spellDescription = "";
 
   Object.values(SummonerSpells.data).forEach((spell) => {
     if (spell.key == id) {
-      spellName = spell.id;
+      spellId = spell.id;
+      spellName = spell.name;
+      spellDescription = spell.description;
     }
   });
 
   return (
-    <Image
-      alt="SummonerSpellIcon"
-      width={64}
-      height={64}
-      src={
-        "http://ddragon.leagueoflegends.com/cdn/" +
-        gameVersion +
-        "/img/spell/" +
-        spellName +
-        ".png"
-      }
-    />
+    <div style={{ width: 64, height: 64 }}>
+      <Tippy
+        theme="translucent"
+        placement="bottom-start"
+        content={
+          <>
+            <span>{spellName}</span>
+            <hr />
+            <span>{spellDescription}</span>
+          </>
+        }
+      >
+        <span>
+          <Image
+            alt="SummonerSpellIcon"
+            width={64}
+            height={64}
+            src={
+              "http://ddragon.leagueoflegends.com/cdn/" +
+              gameVersion +
+              "/img/spell/" +
+              spellId +
+              ".png"
+            }
+          />
+        </span>
+      </Tippy>
+    </div>
   );
 };
 export default SummonerSpellIcon;
